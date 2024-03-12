3a7ec5e405200e973376bb31ad6c51819c9f9340
gjmooney <gjmooney@asu.edu>
2024-03-12 09:33:10 +0100

    Big contructor

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,15 @@ import {
   MainAreaWidget,
   WidgetTracker
 } from '@jupyterlab/apputils';
+import * as THREE from 'three';
+
+//@ts-expect-error AR.js doesn't have type definitions
+import * as THREEx from '@ar-js-org/ar.js/three.js/build/ar-threex.js';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { Widget } from '@lumino/widgets';
-
-interface APODResponse {
-  copyright: string;
-  date: string;
-  explanation: string;
-  media_type: 'video' | 'image';
-  title: string;
-  url: string;
-}
 
 class APODWidget extends Widget {
   /**
@@ -29,71 +26,350 @@ class APODWidget extends Widget {
     super();
 
     this.addClass('my-apodWidget');
+    // promise to track if AR.js has loaded the webcam
+    this.webcam_loaded = new Promise(resolve => {
+      this.resolve = resolve;
+    });
 
-    // Add an image element to the panel
-    this.img = document.createElement('img');
-    this.node.appendChild(this.img);
+    window.addEventListener('arjs-video-loaded', e => {
+      this.resolve();
+    });
 
-    // Add a summary element to the panel
-    this.summary = document.createElement('p');
-    this.node.appendChild(this.summary);
-  }
+    // Set up three stuff
+    console.log('Set up three stuff');
+    this.clock = new THREE.Clock();
+    this.scene = new THREE.Scene();
 
-  /**
-   * The image element associated with the widget.
-   */
-  readonly img: HTMLImageElement;
+    // TODO: Add this as a python option
+    // this.scene.background = new THREE.TextureLoader().load(blueBg);
 
-  /**
-   * The summary text element associated with the widget.
-   */
-  readonly summary: HTMLParagraphElement;
+    this.ambientLight = new THREE.AmbientLight(0x404040, 0.5);
+    this.scene.add(this.ambientLight);
 
-  /**
-   * Handle update requests for the widget.
-   */
-  async updateAPODImage(): Promise<void> {
-    const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`
-    );
+    this.camera = new THREE.Camera();
+    this.scene.add(this.camera);
 
-    if (!response.ok) {
-      const data = await response.json();
-      if (data.error) {
-        this.summary.innerText = data.error.message;
-      } else {
-        this.summary.innerText = response.statusText;
-      }
-      return;
+    // Set up source stuff
+    console.log('Set up source stuff');
+    this.arToolkitSource = new THREEx.ArToolkitSource({
+      sourceType: 'webcam'
+      // source height/width used to set ideal in userMediaConstraints
+      // sourceWidth: this.get('width'),
+      // sourceHeight: this.get('height'),
+      // displayWidth: this.get('width'),
+      // displayHeight: this.get('height')
+    });
+
+    // TODO: resize? Height of cell doesn't change on resize, so to maintain aspect ratio, width of video shouldn't change either
+    this.arToolkitSource.init();
+
+    // Set up context stuff
+    console.log('Set up context stuff');
+    this.arToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl:
+        THREEx.ArToolkitContext.baseURL + '../data/data/camera_para.dat',
+      detectionMode: 'mono'
+    });
+
+    // copy projection matrix to camera when initialization complete
+    this.arToolkitContext.init(() => {
+      this.camera.projectionMatrix.copy(
+        this.arToolkitContext.getProjectionMatrix()
+      );
+    });
+
+    // Set up marker roots
+    console.log('Set up marker roots');
+    this.markerRootArray = [];
+    this.markerGroupArray = [];
+
+    this.patternArray = [
+      'letterA',
+      'letterB',
+      'letterC',
+      'letterD',
+      'letterF',
+      'kanji'
+    ];
+
+    this.rotationArray = [
+      new THREE.Vector3(-Math.PI / 2, 0, 0),
+      new THREE.Vector3(0, -Math.PI / 2, Math.PI / 2),
+      new THREE.Vector3(Math.PI / 2, 0, Math.PI),
+      new THREE.Vector3(-Math.PI / 2, Math.PI / 2, 0),
+      new THREE.Vector3(Math.PI, 0, 0),
+      new THREE.Vector3(0, 0, 0)
+    ];
+
+    for (let i = 0; i < 6; i++) {
+      this.markerRoot = new THREE.Group();
+
+      this.markerRootArray.push(this.markerRoot);
+
+      this.scene.add(this.markerRoot);
+
+      this.markerControls = new THREEx.ArMarkerControls(
+        this.arToolkitContext,
+        this.markerRoot,
+        {
+          type: 'pattern',
+          patternUrl:
+            THREEx.ArToolkitContext.baseURL +
+            'examples/marker-training/examples/pattern-files/pattern-' +
+            this.patternArray[i] +
+            '.patt'
+        }
+      );
+
+      this.markerGroup = new THREE.Group();
+      this.markerGroupArray.push(this.markerGroup);
+
+      this.markerGroup.position.y = -1.25 / 2;
+      this.markerGroup.rotation.setFromVector3(this.rotationArray[i]);
+
+      this.markerRoot.add(this.markerGroup);
     }
 
-    const data = (await response.json()) as APODResponse;
+    // Set up scene stuff
+    console.log('Set up scene stuff');
+    this.sceneGroup = new THREE.Group();
 
-    if (data.media_type === 'image') {
-      // Populate the image
-      this.img.src = data.url;
-      this.img.title = data.title;
-      this.summary.innerText = data.title;
-      if (data.copyright) {
-        this.summary.innerText += ` (Copyright ${data.copyright})`;
+    // a 1x1x1 cube model with scale factor 1.25 fills up the physical cube
+    this.sceneGroup.scale.set(1.75 / 2, 1.75 / 2, 1.75 / 2);
+
+    // Build stage
+    console.log('Build Stage');
+    this.loader = new THREE.TextureLoader();
+
+    // remove old model first
+    if (this.stage) {
+      this.removeFromScene(this.stage);
+    }
+
+    // reversed cube
+    this.stageMesh = new THREE.MeshBasicMaterial({
+      // map: this.stageTexture,
+      color: '#1a1b26',
+      side: THREE.BackSide
+    });
+
+    this.stage = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), this.stageMesh);
+    this.sceneGroup.add(this.stage);
+
+    // Cube edges
+    console.log('Cube edges');
+    this.edgeGroup = new THREE.Group();
+
+    this.edgeGeometry = new THREE.CylinderGeometry(0.03, 0.03, 2, 32);
+
+    this.edgeCenters = [
+      new THREE.Vector3(0, -1, -1),
+      new THREE.Vector3(0, 1, -1),
+      new THREE.Vector3(0, -1, 1),
+      new THREE.Vector3(0, 1, 1),
+      new THREE.Vector3(-1, 0, -1),
+      new THREE.Vector3(1, 0, -1),
+      new THREE.Vector3(-1, 0, 1),
+      new THREE.Vector3(1, 0, 1),
+      new THREE.Vector3(-1, -1, 0),
+      new THREE.Vector3(1, -1, 0),
+      new THREE.Vector3(-1, 1, 0),
+      new THREE.Vector3(1, 1, 0)
+    ];
+
+    this.edgeRotations = [
+      new THREE.Vector3(0, 0, Math.PI / 2),
+      new THREE.Vector3(0, 0, Math.PI / 2),
+      new THREE.Vector3(0, 0, Math.PI / 2),
+      new THREE.Vector3(0, 0, Math.PI / 2),
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(Math.PI / 2, 0, 0),
+      new THREE.Vector3(Math.PI / 2, 0, 0),
+      new THREE.Vector3(Math.PI / 2, 0, 0),
+      new THREE.Vector3(Math.PI / 2, 0, 0)
+    ];
+
+    for (let i = 0; i < 12; i++) {
+      let edge = new THREE.Mesh(
+        this.edgeGeometry,
+        new THREE.MeshLambertMaterial({
+          color: 0x262626
+        })
+      );
+
+      edge.position.copy(this.edgeCenters[i]);
+      edge.rotation.setFromVector3(this.edgeRotations[i]);
+
+      this.edgeGroup.add(edge);
+    }
+
+    this.sceneGroup.add(this.edgeGroup);
+
+    // Load model
+    console.log('Load model');
+    if (!this.gltfLoader) {
+      this.gltfLoader = new GLTFLoader();
+    }
+
+    // remove old model first
+    if (this.gltfModel) {
+      //this.removeFromScene(this.gltfModel);
+      console.log('This is where remove should go');
+    }
+
+    // load model
+    this.gltfLoader.load(
+      this.get('model_url'),
+      gltf => {
+        let scale = this.get('scale');
+        this.gltfModel = gltf.scene;
+        this.gltfModel.scale.set(scale, scale, scale);
+        this.gltfModel.position.fromArray(this.get('position'));
+
+        console.log('gltf', gltf);
+
+        console.log('this.gltfModel', this.gltfModel);
+        this.animations = gltf.animations;
+        this.mixer = new THREE.AnimationMixer(this.gltfModel);
+
+        if (this.animations) {
+          this.animations.forEach(clip => {
+            this.mixer.clipAction(clip).play();
+          });
+        }
+
+        this.sceneGroup.add(this.gltfModel);
+      },
+      () => {
+        console.log('model loading');
+      },
+      error => {
+        console.log('Error loading model', error);
       }
-    } else {
-      this.summary.innerText = 'Random APOD fetched was not an image.';
+    );
+
+    // fancy light
+    this.pointLight = new THREE.PointLight(0xffffff, 1, 50);
+    this.pointLight.position.set(0.5, 3, 2);
+    this.scene.add(this.pointLight);
+
+    // View Stuff
+    // start time for FPS limit
+    this.then = performance.now();
+    this.fpsInterval = 1000 / this.model.get('fps_limit');
+
+    // Check if webcam feed already exists
+    this.webcamFromArjs = document.getElementById('arjs-video');
+
+    // Wait for AR.js to set up webcam feed before rendering view
+    if (!this.webcamFromArjs) {
+      await this.model.webcam_loaded;
+    }
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true
+    });
+
+    this.renderer.setClearColor(new THREE.Color('lightgrey'), 0);
+    this.renderer.setSize(this.model.get('width'), this.model.get('height'));
+    this.renderer.domElement.style.position = 'absolute';
+    this.renderer.domElement.style.top = '0px';
+    this.renderer.domElement.style.left = '0px';
+
+    this.animate();
+  }
+
+  readonly clock: THREE.Clock;
+  readonly scene: THREE.Scene;
+  readonly ambientLight: THREE.AmbientLight;
+  readonly camera: THREE.Camera;
+  arToolkitSource: any;
+  arToolkitContext: any;
+  markerControls: any;
+  readonly markerRootArray: THREE.Group[];
+  readonly markerGroupArray: THREE.Group[];
+  readonly patternArray: string[];
+  readonly rotationArray: THREE.Vector3[];
+  markerRoot: THREE.Group;
+  markerGroup: THREE.Group;
+  readonly sceneGroup: THREE.Group;
+  readonly pointLight: THREE.PointLight;
+  readonly loader: THREE.TextureLoader;
+  readonly stageMesh: THREE.MeshBasicMaterial;
+  readonly stage: THREE.Mesh;
+  readonly edgeGroup: THREE.Group;
+  readonly edgeGeometry: THREE.CylinderGeometry;
+  readonly edgeCenters: THREE.Vector3[];
+  readonly edgeRotations: THREE.Vector3[];
+  gltfLoader: GLTFLoader;
+  gltfModel: any;
+  okToLoadModel: boolean;
+  animations: THREE.AnimationClip[] | undefined;
+  mixer: any;
+  renderer: THREE.WebGLRenderer;
+  animationRequestId: number | undefined;
+  mixerUpdateDelta: number;
+  now: number;
+  then: number;
+  elapsed: number;
+  readonly fpsInterval: number;
+  // observer: IntersectionObserver;
+  existingWebcam: any;
+  newWebcam: any;
+  webcam_loaded: any;
+  resolve: any;
+
+  animate() {
+    this.animationRequestId = window.requestAnimationFrame(
+      this.animate.bind(this)
+    );
+
+    this.mixerUpdateDelta = this.model.clock.getDelta();
+
+    this.now = performance.now();
+
+    // time elapsed since last frame
+    // TODO: I think I can use getDelta from the three clock here maybe?
+    this.elapsed = this.now - this.then;
+
+    // if enough time has passed to render the next frame
+    if (this.elapsed > this.fpsInterval) {
+      this.then = this.now - (this.elapsed % this.fpsInterval);
+
+      this.update();
+      this.model.mixer.update(this.mixerUpdateDelta);
+
+      this.renderer.render(this.model.scene, this.model.camera);
     }
   }
 
-  /**
-   * Get a random date string in YYYY-MM-DD format.
-   */
-  randomDate(): string {
-    const start = new Date(2010, 1, 1);
-    const end = new Date();
-    const randomDate = new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    );
-    return randomDate.toISOString().slice(0, 10);
+  update() {
+    // update artoolkit on every frame
+    if (this.model.arToolkitSource.ready !== false)
+      this.model.arToolkitContext.update(this.model.arToolkitSource.domElement);
+
+    for (let i = 0; i < 6; i++) {
+      if (this.model.markerRootArray[i].visible) {
+        this.model.markerGroupArray[i].add(this.model.sceneGroup);
+        // console.log("visible: " + this.model.patternArray[i]);
+        break;
+      }
+    }
   }
 }
+window.addEventListener('markerFound', () => {
+  console.log('marker found lisener');
+  console.log('Marker found');
+});
+
+window.addEventListener('markerLost', () => {
+  console.log('marker lost lisener');
+  console.log('Marker lost');
+});
 
 /**
  * Activate the APOD widget extension.
