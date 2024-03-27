eb15a4df841080fd46b98b77846469c29563b380
gjmooney <gjmooney@asu.edu>
2024-03-27 11:59:07 +0100

    Start presentation layout

---
+++ src/layouts/PeerSidePane.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const PeerSidePane = () => {};
+export default PeerSidePane;
---
+++ src/layouts/PresenterView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PresenterView = () => {
+  return (
+    <div className="presenter-container-main">
+      <div className="presenter-container">
+        <div className="presenter-video"></div>
+      </div>
+      <div className="peer-side-pane"></div>
+    </div>
+  );
+};
+export default PresenterView;
